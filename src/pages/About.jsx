2662eac32047b20ad51a8a89e56e480db07ecb69
omkar mhaske <135412726+omkarmaske160@users.noreply.github.com>
2024-06-05 13:43:16 +0530

    update

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,19 +1,48 @@
 import React from 'react'
 
 const About = () => {
+    const userDetails = [
+        { key: 'Name', value: 'Omkar Makse' },
+        { key: 'Date of birth', value: '03 Augest 2003' },
+        { key: 'Address', value: 'Sai Palace Aurangpura' },
+        { key: 'Zip code', value: '431001' },
+        { key: 'Email', value: 'omkarmaske160@gmail.com' },
+        { key: 'Phone', value: '+91 8080328556' }
+    ];
     return <>
-        <section id="about" className="bg-gray-100 py-16 px-4 md:px-8">
-            <h2 className="text-3xl font-bold mb-8">About Me</h2>
-            <p className="max-w-2xl text-gray-600  mb-8">
-                I am a passionate full-stack web developer with a strong background in building modern, responsive, and
-                scalable web applications. I have experience working with a variety of programming languages, frameworks, and
-                technologies, and I am always eager to learn new skills and techniques to improve my craft.
-            </p>
-            <p className="max-w-2xl text-gray-600  mb-8">
-                In my free time, I enjoy exploring new technologies, contributing to open-source projects, and staying
-                up-to-date with the latest industry trends. I am also an avid reader and enjoy hiking and traveling.
-            </p>
-        </section>
+        <div className=" text-white p-8 w-full md:mt-10 md:mb-16">
+            <div className="max-w-screen-lg mx-auto">
+                <div className="flex flex-col md:flex-row items-center md:items-start">
+                    <div className="flex-shrink-0 mb-6 md:mb-0 md:mr-6 w-1/2">
+                        <img
+                            src="https://img.freepik.com/free-vector/blond-man-with-eyeglasses-icon-isolated_24911-100831.jpg?t=st=1717569749~exp=1717573349~hmac=96800c7d7cc725a97140211ee6875c8dfc2625a4a4f1f82c0acb3c2bcab6b08c&w=740" // Replace with the actual image source if available
+                            alt="Omkar Maske"
+                            className="w-full rounded-full"
+                        />
+                    </div>
+                    <div className=" md:w-1/2 w-full">
+                        <h1 className=" about-heading w-full ">About Me</h1>
+                        <p className="text-gray-400 mb-4 md:pt-5">
+                            Hello, I'm a MERN stack developer specializing in React and Node.js. I thrive on crafting responsive, efficient web applications that deliver seamless user experiences
+                        </p>
+                        <div className="space-y-4 w-full mt-10 ">
+                            {userDetails.map((detail, index) => (
+                                <div key={index} className=" w-full flex  gap-5">
+                                    <span className="font-bold w-[25%] text-yellow-500">{detail.key} :</span>
+                                    <span >{detail.value}</span>
+                                </div>
+                            ))}
+                        </div>
+
+                        <div className='w-full flex justify-center mt-5 md:justify-start'>
+                            <button className="bg-yellow-500  mt-7  hover:text-slate-700 hover:bg-yellow-400 transition-all duration-300 font-bold text-black px-5 py-3 rounded-full">
+                                DOWNLOAD CV
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </>
 }
 
