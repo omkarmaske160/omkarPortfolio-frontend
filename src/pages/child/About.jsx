985eb18c8f1c3cce81b6d866653fe91bb7eec582
omkar mhaske <135412726+omkarmaske160@users.noreply.github.com>
2024-07-08 19:08:58 +0530

    porfile photo change

--- src/pages/child/About.jsx
+++ src/pages/child/About.jsx
@@ -34,21 +34,25 @@ const About = () => {
             transition: { duration: 1, ease: 'easeOut', delay: 1 },
         },
     };
+    const openWhatsApp = () => {
+        window.open('https://wa.me/919404982960', '_blank');
+    };
 
     return (
         <div id="resume" ref={ref} className="text-white p-8 w-full md:mb-16">
             <div className="max-w-screen-lg mx-auto">
                 <div className="flex flex-col md:flex-row items-center md:items-start md:gap-10">
                     <motion.div
-                        className="flex-shrink-0 mb-6 md:mb-0 md:mr-6 w-1/2"
+                        className="flex-shrink-0 mb-6 md:mb-0 md:mr-6 w-full md:w-1/2"
                         initial="hidden"
                         animate={inView ? "visible" : "hidden"}
                         variants={imageVariants}
                     >
                         <img
-                            src="https://img.freepik.com/free-vector/blond-man-with-eyeglasses-icon-isolated_24911-100831.jpg?t=st=1717569749~exp=1717573349~hmac=96800c7d7cc725a97140211ee6875c8dfc2625a4a4f1f82c0acb3c2bcab6b08c&w=740" // Replace with the actual image source if available
+                            // src="https://img.freepik.com/free-vector/blond-man-with-eyeglasses-icon-isolated_24911-100831.jpg?t=st=1717569749~exp=1717573349~hmac=96800c7d7cc725a97140211ee6875c8dfc2625a4a4f1f82c0acb3c2bcab6b08c&w=740" // Replace with the actual image source if available
+                            src="./user-img.jpg"
                             alt="Omkar Maske"
-                            className="w-full rounded-full"
+                            className="w-full rounded-full md:mt-20"
                         />
                     </motion.div>
                     <motion.div
@@ -70,12 +74,18 @@ const About = () => {
                             ))}
                         </div>
 
-                        <div className='w-full flex justify-center mt-5 md:justify-start'>
+                        <div className='w-full flex justify-center gap-5 mt-5 md:justify-start'>
                             <button
                                 onClick={() => window.location.href = 'https://drive.google.com/uc?export=download&id=1raH45wO3PxyJSiEyLOJJQRV1tK-_EwuP'}
                                 className="bg-yellow-500 mt-7 hover:text-slate-700 hover:bg-yellow-400 transition-all duration-300 font-bold text-black px-5 py-3 rounded-full"
                             >
                                 DOWNLOAD CV
+                            </button>
+                            <button
+                                onClick={() => window.open('https://wa.me/918080328556', '_blank')}
+                                className="bg-green-700 mt-7 hover:text-slate-700 hover:bg-green-600 transition-all duration-300 font-bold text-black px-5 py-3 rounded-full"
+                            >
+                                Whatsapp Me
                             </button>
                         </div>
                     </motion.div>
